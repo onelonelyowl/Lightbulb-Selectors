5cc8df3c5689f5b4f82cb4f17d70cec8f81ffdfe
onelonelyowl <64985946+onelonelyowl@users.noreply.github.com>
2023-10-17 01:29:10 +0100

    completed with hover and counters on all 3

--- script.js
+++ script.js
@@ -1,1 +1,59 @@
-// Write your code here
+let lightbulb1 = document.querySelector("#lightbulb1")
+let lightbulb2 = document.querySelector("#lightbulb2")
+let lightbulb3 = document.querySelector("#lightbulb3")
+let subtitle = document.querySelector("h3.subtitle")
+let count = 0
+let lightbulbs = document.querySelectorAll(".lightbulb")
+
+lightbulb1.addEventListener("click", function(){
+    count++
+    lightbulb1.classList.toggle("active")
+    if(count !== 1){
+        subtitle.innerHTML = `You've clicked the lights ${count} times`
+    }
+    else{
+        subtitle.innerHTML = `You've clicked the lights 1 time`
+    }
+})
+for(i=0; i < lightbulbs.length; i++){
+    let lightbulb = lightbulbs[i] // if i didn't do this and instead just tried to use lightbulbs[i] in the loop it didn't work, idk why
+    lightbulb.addEventListener("mouseenter", function(){
+        count++
+        lightbulb.classList.toggle("active")
+        if(count !== 1){
+            subtitle.innerHTML = `You've clicked the lights ${count} times`
+        }
+        else{
+            subtitle.innerHTML = `You've clicked the lights 1 time`
+        }
+    })
+    lightbulb.addEventListener("mouseleave", function(){
+        lightbulb.classList.toggle("active")
+    })
+}
+
+     
+/*
+lightbulb2.addEventListener("mouseenter", function(){
+    count++
+    lightbulb2.classList.toggle("active")
+    if(count !== 1){
+        subtitle.innerHTML = `You've clicked the lights ${count} times`
+    }
+    else{
+        subtitle.innerHTML = `You've clicked the lights 1 time`
+    }
+})
+lightbulb2.addEventListener("mouseleave", function(){
+    lightbulb2.classList.toggle("active")
+})
+lightbulb3.addEventListener("click", function(){
+    count++
+    lightbulb3.classList.toggle("active")
+    if(count !== 1){
+        subtitle.innerHTML = `You've clicked the lights ${count} times`
+    }
+    else{
+        subtitle.innerHTML = `You've clicked the lights 1 time`
+    }
+})*/
